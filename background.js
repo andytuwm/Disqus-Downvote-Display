464cd4cdda50e44d8b0a9ae753b55d3b5b905a9f
Andy Tu <andytu@live.ca>
2015-01-01 17:02:10 -0800

    create and insert DOM element, pull out the downvote count stored in class name

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
-
+revealDownvotes();
 
 function revealDownvotes() {
 
   var downvotes = document.getElementsByClassName("vote-down");
 
-  for ( var i = 0 ; i < downvotes.length ; i++ ) {
+  for (var i = 0; i < downvotes.length; i++ ) {
 
     var downvoteCount = downvotes[i].getElementById("downvotes");
 
-    if (downvoteCount != )
+    if (downvoteCount != null) {
+      downvoteCount = downvoteCount[0];
+    } else {
+      downvoteCount = document.createElement("span");
+      downvoteCount.id = "downvotes";
+      downvotes[i].insertBefore(downvoteCount, downvotes[i].firstChild);
+    }
+
+    downvoteCount.innerHTML = downvotes[i].className.split("-")[2];
+
+    if (downvoteCount.innerHTML == 0)
+      downvoteCount.style.display = "none";
 
   }
 
