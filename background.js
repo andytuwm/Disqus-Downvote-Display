ba09832d20cec4aae130643a9ff2217431207fc5
Andy Tu <andytu@live.ca>
2015-01-01 18:18:35 -0800

    Upon loading a page with disqus plugin, run the script 5 times to ensure downvotes are displayed but not continuously running afterwards.

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-revealDownvotes();
+var counter = 0; // Track how many times function has ran.
+var startProcess = setInterval(revealDownvotes,3000);
 
 function revealDownvotes() {
 
@@ -10,8 +11,9 @@ function revealDownvotes() {
     var downvoteCount = downvotes[i].getElementsByClassName("downvotes");
 
     // Check if the downvote count element has already been created
-    // or not for whatever reason
-    if ( downvoteCount.length == 0) {
+    // or not, since we'll call the function a number of times because
+    // we do not know when the disqus plugin will load. Like damn it takes forever
+    if ( downvoteCount.length === 0) {
       // Since element does not yet exist, create it and give it an id.
       downvoteCount = document.createElement("span");
       downvoteCount.className = "downvotes";
@@ -31,7 +33,11 @@ function revealDownvotes() {
     downvoteCount.innerHTML = downvotes[i].className.split("-")[2];
 
     // If no downvotes, don't display the 0.
-    if (downvoteCount.innerHTML == "0")
+    if (downvoteCount.innerHTML === "0")
       downvoteCount.style.display = "none";
   }
+
+  // After function has run 5 times, stop running to reduce load.
+  if (++counter === 5)
+    clearInterval(startProcess);
 }
