dccbb571be876634e47b1893e37338b05d452681
Andy Tu <andytuwm@gmail.com>
2015-01-01 16:37:49 -0800

    add manifest.json and initial code

---
+++ background.js
@@ -0,0 +1,15 @@
+
+
+function revealDownvotes() {
+
+  var downvotes = document.getElementsByClassName("vote-down");
+
+  for ( var i = 0 ; i < downvotes.length ; i++ ) {
+
+    var downvoteCount = downvotes[i].getElementById("downvotes");
+
+    if (downvoteCount != )
+
+  }
+
+}
