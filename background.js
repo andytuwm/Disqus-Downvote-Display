72ecbd3e0d8379273ede71a1014570847ea49734
Andy Tu <andytu@live.ca>
2015-01-01 18:03:19 -0800

    format created element, change element detection to using classname because id wouldn't work, add comments, edit manifest.json

--- background.js
+++ background.js
@@ -2,25 +2,36 @@ revealDownvotes();
 
 function revealDownvotes() {
 
+  // Get all elements starting with this class name.
   var downvotes = document.getElementsByClassName("vote-down");
 
   for (var i = 0; i < downvotes.length; i++ ) {
 
-    var downvoteCount = downvotes[i].getElementById("downvotes");
+    var downvoteCount = downvotes[i].getElementsByClassName("downvotes");
 
-    if (downvoteCount != null) {
-      downvoteCount = downvoteCount[0];
-    } else {
+    // Check if the downvote count element has already been created
+    // or not for whatever reason
+    if ( downvoteCount.length == 0) {
+      // Since element does not yet exist, create it and give it an id.
       downvoteCount = document.createElement("span");
-      downvoteCount.id = "downvotes";
+      downvoteCount.className = "downvotes";
+      // Define the element's position. Normal position is a bit off.
+      downvoteCount.style.position = "relative";
+      downvoteCount.style.top = "-3px";
+      // Insert element into DOM.
       downvotes[i].insertBefore(downvoteCount, downvotes[i].firstChild);
+    } else {
+      // If already created, take the first instance.
+      downvoteCount = downvoteCount[0];
     }
 
+    // Pull out value of downvote count and insert into the innerHMTL of the
+    // created element. String is split with '-' because that's how the
+    // classes are named.
     downvoteCount.innerHTML = downvotes[i].className.split("-")[2];
 
-    if (downvoteCount.innerHTML == 0)
+    // If no downvotes, don't display the 0.
+    if (downvoteCount.innerHTML == "0")
       downvoteCount.style.display = "none";
-
   }
-
 }
